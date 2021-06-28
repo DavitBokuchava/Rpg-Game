1a1910bf6672981e7c690ad1ceffb39b9cc81e8f
DavitBokuchava <davitbokuchava@gmail.com>
2021-06-28 18:28:46 +0400

    commit-main-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("  Hi, it is a first lesson of Js Marathon Pro Edition ")
+import './index.scss'
+console.log('  Hi, it is a first lesson of Js Marathon Pro Edition ');
+console.log('###########  change something ');
